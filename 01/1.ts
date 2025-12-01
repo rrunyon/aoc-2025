ba8ecd624556d3356a211038c2e718ed0e314c8e
Rick Runyon <rick.runyon@gmail.com>
2025-12-01 12:11:41 -0500

    01/1

--- 01/1.ts
+++ 01/1.ts
@@ -1,8 +1,25 @@
 import { readFile } from 'fs/promises';
 
 async function solution() {
-  let input = (await readFile('./01/input.txt', 'utf8')).split('\n');
+  let input = (await readFile('./01/input.txt', 'utf8')).split('\n') as Array<string>;
 
+  let dial = 50;
+  let zeroCount = 0;
+  
+  for (const rotation of input) {
+    let direction = rotation.charAt(0);
+    let turns = Number(rotation.slice(1));
+
+    if (direction === 'L') {
+      dial = (dial - turns + 100) % 100;
+    } else {
+      dial = (dial + turns) % 100;
+    }
+
+    if (dial === 0) zeroCount++;
+  }
+
+  return zeroCount;
 }
 
-await solution();
+console.log(await solution());
