b0bcf284ce6a3be06c8c045826129e0484d8cdf0
Rick Runyon <rick.runyon@gmail.com>
2025-12-04 16:36:48 -0500

    03/2

--- 03/2.ts
+++ 03/2.ts
@@ -3,34 +3,48 @@ import { readFile } from 'fs/promises';
 async function solution() {
   let input = (await readFile('./03/input.txt', 'utf8')).split('\n') as Array<string>;
   
-  let result = 0;
+  let result = BigInt(0);
 
   for (let bank of input) {
     const joltage = getMaxJoltage(bank.split(''));
-    console.log(joltage);
-    result += joltage;
+    result += BigInt(joltage);
   }
 
   return result;
 }
 
-function getMaxJoltage(bank: String[], currentIndex = 0, currentConfiguration: String[] = [], currentMax = 0): number {
+function getMaxJoltage(bank: String[], currentIndex = 0, currentConfiguration: String[] = [], cache = new Map): number {
+  const current = currentConfiguration.join('');
+
   if (currentConfiguration.length === 12) {
-    return Number(currentConfiguration.join(''));
+    let currentJoltage = Number(currentConfiguration.join(''));
+    if (!cache.get('max') || currentJoltage > cache.get('max')) {
+      cache.set('max', currentJoltage);
+    }
+
+    return 0;
   }
+
+  if (currentConfiguration.length > 0 && cache.get('max')) {
+    let potentialJoltageStr = currentConfiguration.join('');
+    while (potentialJoltageStr.length < 12) potentialJoltageStr += '9';
+    let potentialJoltage = Number(potentialJoltageStr);
+    if (cache.get('max') > potentialJoltage) return 0;
+  }
+
+  if (currentConfiguration.length + (bank.length - currentIndex + 1) < 12) return 0;
 
   if (currentIndex >= bank.length) return 0;
 
-  let left = getMaxJoltage(bank, currentIndex + 1, currentConfiguration, currentMax);
+  getMaxJoltage(bank, currentIndex + 1, currentConfiguration, cache);
 
-  let right = 0;
   let currentBattery = bank[currentIndex];
   if (currentBattery) {
     let nextConfiguration = [...currentConfiguration, currentBattery];
-    right = getMaxJoltage(bank, currentIndex + 1, nextConfiguration);
+    getMaxJoltage(bank, currentIndex + 1, nextConfiguration, cache);
   }
 
-  return Math.max(left, right);
+  return cache.get('max');
 }
 
 console.log(await solution());
