2e3838b096a5a9764d8283a9b644af9e90f46f0b
Rick Runyon <rick.runyon@gmail.com>
2025-12-04 13:49:45 -0500

    03/2 WIP

--- 03/2.ts
+++ 03/2.ts
@@ -1,53 +1,36 @@
 import { readFile } from 'fs/promises';
 
 async function solution() {
-  let input = (await readFile('./02/input.txt', 'utf8')).split('\n') as Array<string>;
+  let input = (await readFile('./03/input.txt', 'utf8')).split('\n') as Array<string>;
   
-  let ranges: Array<[number, number]> = [];
-  for (const line of input) {
-    const rangeStrings = line.split(',');
+  let result = 0;
 
-    for (const range of rangeStrings) {
-      let [left, right] = range.split('-').map(Number);
-      if (left && right) ranges.push([left, right]);
-    }
-  }
-
-  let result = BigInt(0);
-
-  for (const [rangeMin, rangeMax] of ranges) {
-    for (let i = rangeMin; i <= rangeMax; i++) {
-      if (isInvalid(i)) result += BigInt(i);
-    }
+  for (let bank of input) {
+    const joltage = getMaxJoltage(bank.split(''));
+    console.log(joltage);
+    result += joltage;
   }
 
   return result;
 }
 
-function isInvalid(id: number): boolean {
-  let idString = String(id);
-
-  for (let i = 1; i < idString.length; i++) {
-    if (idString.length % i === 0) {
-      let current = idString.slice(0, i);
-      let next = "";
-      let allMatching = true;
-
-      for (let j = i; j < idString.length; j += i) {
-        next = current;
-        current = idString.slice(j, i + j);
-
-        if (next !== current) {
-          allMatching = false;
-          break;
-        }
-      }
-
-      if (allMatching) return true;
-    }
+function getMaxJoltage(bank: String[], currentIndex = 0, currentConfiguration: String[] = [], currentMax = 0): number {
+  if (currentConfiguration.length === 12) {
+    return Number(currentConfiguration.join(''));
   }
 
-  return false;
+  if (currentIndex >= bank.length) return 0;
+
+  let left = getMaxJoltage(bank, currentIndex + 1, currentConfiguration, currentMax);
+
+  let right = 0;
+  let currentBattery = bank[currentIndex];
+  if (currentBattery) {
+    let nextConfiguration = [...currentConfiguration, currentBattery];
+    right = getMaxJoltage(bank, currentIndex + 1, nextConfiguration);
+  }
+
+  return Math.max(left, right);
 }
 
 console.log(await solution());
